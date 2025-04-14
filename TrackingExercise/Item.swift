b8b804a93b69ab4c9350646e963cae668f83393d
yujian <645113027@qq.com>
2025-04-14 07:47:44 -0400

    Initial Commit

---
+++ TrackingExercise/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TrackingExercise
+//
+//  Created by Yujian Song on 4/14/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TrackingExerciseTests/TrackingExerciseTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackingExerciseTests.swift
+//  TrackingExerciseTests
+//
+//  Created by Yujian Song on 4/14/25.
+//
+
+import Testing
+@testable import TrackingExercise
+
+struct TrackingExerciseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
